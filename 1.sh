8c42f6960f4fb882ce3b05c1f8582e8811dcbe7c
parkxu465 <1287580@qq.com>
2024-12-18 07:03:42 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-cd ccminer
-./start.sh
+yes | pkg update -y
+yes | pkg upgrade -y
+yes | pkg install libjansson wget nano -y
+
+mkdir ccminer && cd ccminer
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
+wget https://raw.githubusercontent.com/parkxu465/parkxuverus/main/config.json
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
+chmod +x ccminer start.sh
+echo "cd ccminer/&&/start.sh" >> ../usr/etc/bash.bashrc
