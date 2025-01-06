74ca7a74c9a0af7c771787eae0cbc4f9c9e247e6
parkxu465 <1287580@qq.com>
2025-01-06 21:59:52 +0200

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+pkg install root-repo
+pkg install x11-repo
+yes | pkg update -y
+yes | pkg upgrade -y
+yes | pkg install libjansson wget nano -y
+
+mkdir ccminer 
+cd ccminer
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer
+wget https://raw.githubusercontent.com/parkxu465/parkxuverus/main/config1.json
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/generic/start.sh
+chmod +x ccminer start.sh
+cd
+cd
+cd
+echo "cd ccminer/&&./start.sh" >> ../usr/etc/bash.bashrc
